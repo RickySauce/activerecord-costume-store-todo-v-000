6907ab8a32e20aec7a09721a0e5a66457f985db8
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-27 11:01:40 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouse
+
+  def change
+    create_table :haunted_houses do |h|
+      h.text :name
+      h.text :location
+      h.text :theme
+      h.float :price
+      h.boolean :family_friendly
+      h.datetime :opening_date
+      h.datetime :closing_date
+      h.text :long_description
+    end
+  end
+
+end
