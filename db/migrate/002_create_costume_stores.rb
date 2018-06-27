d4c39aa192e8325e764273d4c0fe984af5fa0dc1
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-27 10:57:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costume_stores do |c|
+      c.text :name
+      c.text :location
+      c.integer :costume_inventory
+      c.integer :number_of_employees
+      c.boolean :still_open?
+      c.float :opening_time
+      c.float :closing_time
+    end
+  end
+  
+end
